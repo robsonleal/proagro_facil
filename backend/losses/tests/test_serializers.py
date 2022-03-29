32f36d8a53d5658e5ca60af336e360d2c40cced2
Robson Leal <devleal@hotmail.com>
2022-03-29 14:01:02 -0300

    Criando teste para serializer de comunicacao de perdas

---
+++ backend/losses/tests/test_serializers.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from losses.models import CropLoss
+from losses.serializers import CropLossSerializer
+
+
+class CropLossSerializerTestCase(TestCase):
+    def setUp(self):
+        self.crop_loss = CropLoss.objects.create(
+            farmer_name='Testing Name',
+            farmer_email='example@testing.com',
+            farmer_cpf='222-111-111-15',
+            crop_lat=-53.00000,
+            crop_long=-14.00000,
+            crop_type='Milho',
+            harvest_date='2022-10-10',
+            event_croploss='CHEX',
+        )
+        self.serializer = CropLossSerializer(instance=self.crop_loss)
+
+    def test_check_serializer_fields(self):
+        data = self.serializer.data
+        self.assertEqual(
+            set(data.keys()),
+            set([
+                'id',
+                'farmer_name',
+                'farmer_email',
+                'farmer_cpf',
+                'crop_lat',
+                'crop_long',
+                'crop_type',
+                'harvest_date',
+                'event_croploss'
+            ]))
