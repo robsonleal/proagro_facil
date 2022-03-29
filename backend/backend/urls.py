7681e44721cc1aee4daec4c7dec64cf4afc20806
Robson Leal <devleal@hotmail.com>
2022-03-29 17:36:46 -0300

    Correcao caminho URL

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,7 +25,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
-    path('perdas/', include('losses.urls')),
+    path('', include('losses.urls')),
     path(
         'swagger/',
         schema_view.with_ui('swagger', cache_timeout=0),
