2773ab86e78316bc3d33377c8fd3d19b768f533a
Robson Leal <devleal@hotmail.com>
2022-03-29 15:49:51 -0300

    Criando view com authenticaçao basica para API

--- backend/losses/views.py
+++ backend/losses/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from losses.models import CropLoss
+from losses.serializers import CropLossSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class CropLossViewSet(viewsets.ModelViewSet):
+    queryset = CropLoss.objects.all()
+    serializer_class = CropLossSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
