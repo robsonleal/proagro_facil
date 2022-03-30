51b6eacb3c237529abe9fc5717ae288661cdd28d
Robson Leal <devleal@hotmail.com>
2022-03-30 14:32:29 -0300

    Criando modelo da API que vai ser recebida na aplicacao frontend

---
+++ frontend/src/app/models/croploss.model.spec.ts
@@ -0,0 +1,7 @@
+import { Croploss } from './croploss.model';
+
+describe('Croploss', () => {
+  it('should create an instance', () => {
+    expect(new Croploss()).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/models/croploss.model.ts
@@ -0,0 +1,11 @@
+export class Croploss {
+    id?: any;
+    farmer_name?: string;
+    farmer_email?: string;
+    farmer_cpf?: boolean;
+    crop_lat?: number;
+    crop_long?: number;
+    crop_type?: string;
+    harvest_date?: string;
+    event_croploss?: string;
+}
